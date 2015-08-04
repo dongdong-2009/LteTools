1c46171b6b3fde02d501cfb29883a40f6ee71cae
ouyh18ctc <ouyh19@189.cn>
2015-08-05 07:49:19 +0800

    2015-08-05-b

--- Lte.WebApp/Controllers/Topic/CollegeQueryController.cs
+++ Lte.WebApp/Controllers/Topic/CollegeQueryController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Linq;
 using System.Web.Http;
 using Lte.Parameters.Region.Abstract;
 using Lte.Parameters.Region.Entities;
